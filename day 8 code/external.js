f7202daf9be40bcf11058f387d5a9b5b759d7732
kumar-max-bit <kumarking6191@gmail.com>
2026-04-30 20:20:25 +0530

    day 8 code

---
+++ day 8 code/external.js
@@ -0,0 +1,79 @@
+num="java"
+num=50;
+console.log(num);
+//
+
+num=true
+console.log(num);
+//
+
+class sampleexample{
+
+    display(){
+        console.log("this is the example of this");
+    }
+    test(){
+        console.log("test method");
+    }
+}
+
+
+s1=new sampleexample()
+s1.display()
+s1.test()
+
+//
+//
+//function addition(){
+    console.log(10+20);
+//}
+//addition()
+//
+
+
+num=331;
+
+result=num%2==0?"Even Number":"Odd Number"
+console.log(result);
+//
+a=10;
+b='10';
+c=1;
+d=true;
+console.log(c==d);
+console.log(c===d);
+
+
+a=10;
+b='10';
+c=null
+
+d=Symbol(10);
+e=65465342767n
+console.log("Type of varible a is:",typeof a);
+console.log("Type of varible b is:",typeof b);
+console.log("Type of varible c is:",typeof c);
+console.log("Type of varible d is:",typeof d);
+console.log("Type of varible e is:",typeof e);
+//
+// Array example
+let fruits = ["Apple", "Banana", "Mango"];
+console.log("Array elements:", fruits);
+console.log("Type of fruits:", typeof fruits);
+
+// Object example
+let person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25
+};
+console.log("Person Object:", person);
+console.log("First Name:", person.firstName);
+
+// Function with parameters
+function greet(name) {
+    console.log("Hello " + name + ", welcome to JavaScript!");
+}
+greet("Kumar");
+
+
